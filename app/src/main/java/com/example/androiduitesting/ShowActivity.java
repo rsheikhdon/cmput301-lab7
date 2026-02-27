2daa1755753b2a6089ea993028816fb205e0e838
rsheikhdon <rsheikhdon124@gmail.com>
2026-02-27 14:35:07 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,23 @@
+package com.example.androiduitesting;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        TextView cityText = findViewById(R.id.text_city_name);
+        Button backButton = findViewById(R.id.button_back);
+
+        String city = getIntent().getStringExtra("city_name");
+        cityText.setText(city);
+
+        backButton.setOnClickListener(v -> finish());
+    }
+}
